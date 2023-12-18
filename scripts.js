b50aba39285c9355feadee132731ad000c395080
Norbert Schnell <norbert.schnell@hs-furtwangen.de>
2023-12-19 00:03:17 +0100

    looking for rigth filter coefficient

--- scripts.js
+++ scripts.js
@@ -105,7 +105,7 @@ function onDeviceMotion(e) {
   }
 
   if (filterCoeff === 0) {
-    filterCoeff = Math.exp(-2.0 * Math.PI * e.interval / 0.5);
+    filterCoeff = Math.exp(-2.0 * Math.PI * e.interval / 1);
   }
 
   const acc = scaleAcc * e.acceleration.x;
